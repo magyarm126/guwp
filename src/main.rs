81612986267326cfb613022ab0b12d6946799fc0
Magyar Máté <magyarmate126@gmail.com>
2026-08-19 19:58:06 +0200

    fixed limits for compatibility

--- src/main.rs
+++ src/main.rs
@@ -81,7 +81,7 @@ impl State {
             .request_device(&wgpu::DeviceDescriptor {
                 label: None,
                 required_features: wgpu::Features::empty(),
-                required_limits: wgpu::Limits::downlevel_defaults(),
+                required_limits: wgpu::Limits::default(),
                 experimental_features: wgpu::ExperimentalFeatures::disabled(),
                 memory_hints: wgpu::MemoryHints::MemoryUsage,
                 trace: wgpu::Trace::Off,
