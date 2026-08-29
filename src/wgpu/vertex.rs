e487ad4089652d1ee7f78d2a644dc50044df1848
Magyar Máté <magyarmate126@gmail.com>
2026-08-29 05:22:39 +0200

    Triangle prep

--- src/wgpu/vertex.rs
+++ src/wgpu/vertex.rs
@@ -1,4 +1,44 @@
+use bytemuck::{Pod, Zeroable};
+
+/*
+The #[repr(C)] attribute tells Rust to lay out the struct exactly as a C compiler would, with no padding surprises.
+Pod (plain old data) and Zeroable from bytemuck certify that the bytes can be safely cast to a &[u8] without undefined behaviour.
+ */
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Pod, Zeroable)]
 struct Vertex {
-    position: [f32; 2],
-    color: [f32; 4],
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+impl Vertex {
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        wgpu::VertexBufferLayout {
+            array_stride: size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttribute { //pos
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute { //color
+                    offset: size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+            ],
+        }
+    }
+
+    //.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+
+    fn get_tringle_vector() -> Vec<Vertex> {
+        vec![
+            Vertex { position: [0.0,  0.5, 0.0], color: [1.0, 0.0, 0.0] },
+            Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
+            Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] },
+        ]
+    }
 }
